33e8b758ce32b6faac4c7d60837e8c74e5721d66
OUNZAR Aymane <aymane.ounzar@imt-atlantique.net>
2025-03-17 14:13:12 +0100

    Simple embedder that only works with text

--- embedder.py
+++ embedder.py
@@ -1,14 +1,11 @@
-from langchain_community.document_loaders import TextLoader
-from langchain_mistralai.chat_models import ChatMistralAI
 from langchain_mistralai.embeddings import MistralAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
-from langchain.document_loaders import UnstructuredPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 import time
-from PyPDF2 import PdfReader
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers import AutoTokenizer
 
@@ -19,7 +16,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def load_and_split(pdf):
-    loader = UnstructuredPDFLoader(pdf, strategy="hi-res")
+    loader = PyPDFLoader(pdf)
     documents = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     chunks = text_splitter.split_documents(documents)
@@ -34,6 +31,6 @@ def embedd(chunks):
 
 
 if __name__ == '__main__' :
-    chunks = load_and_split(r"C:\Users\ounza\IMT\Semestre 6\PRONTO\ai-agent\docs\astro-procedures-resume-anon.pdf")
+    chunks = load_and_split(r"C:\Users\ounza\IMT\Semestre 6\PRONTO\ai-agent\docs\Le-coronographe-de-Lyot.pdf")
     print(chunks)
     embedd(chunks)
