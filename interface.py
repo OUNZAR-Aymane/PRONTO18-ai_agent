821436d32caca05587a494b10a9999279dcabd17
OUNZAR Aymane <aymane.ounzar@imt-atlantique.net>
2025-03-20 01:35:21 +0100

    Amélioration de l'interface

--- interface.py
+++ interface.py
@@ -1,6 +1,14 @@
 import streamlit as st
 from chatBot import get_response
 from embedder import embedd, load_and_split
+from langchain_core.messages import HumanMessage ,AIMessage
+
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
+
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
 # Titre de la page
 st.set_page_config(page_title="Chatbot Observatoire Astronomique", page_icon=":astronaut:", layout="wide")
@@ -26,20 +34,26 @@ with st.sidebar:
         chunks = load_and_split(pdf)
         embedd(chunks)
 
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
 # Chatbot - Interface d'entrée de texte
-user_input = st.text_input("Pose ta question ici:")
+user_input = st.chat_input("Pose ta question ici:")
 
 if user_input:
-    # Réponse simple pour l'instant
-    st.write(f"Tu as demandé: {user_input}")
+    with st.chat_message("user"):
+        st.write(user_input)
+    st.session_state.messages.append({"role": "user", "content": user_input})
     # Ici, tu pourrais ajouter un modèle NLP pour répondre à la question ou simuler une réponse intelligente.
-    st.write(get_response(user_input))
+    with st.spinner("Réponse en cours de génération"):
+        response = get_response(user_input,st.session_state['chat_history'])
+        st.session_state["chat_history"].append(HumanMessage(content=user_input))
+        st.session_state["chat_history"].append(AIMessage(content=response))
+    with st.chat_message("assistant"):
+        st.write(response)
+    st.session_state.messages.append({"role": "assistant", "content": response})
 
-# Ajouter un footer
-st.markdown("""
-    ---
-    Fait avec par l'équipe du projet PRONTO 18.
-""")
 
 
 
